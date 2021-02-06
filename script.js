4cbdc739335bd0423d61d07e776b620993a23d59
sabbirquince <59896171+sabbirquince@users.noreply.github.com>
2021-02-07 00:14:36 +0600

    hid element for new search function added

--- script.js
+++ script.js
@@ -1,33 +1,46 @@
+//////////// BUTTON EVENT HANDLER ////////////
 const inputButton = document.getElementById("input-button");
 
 inputButton.addEventListener("click", () => {
+  const mealBoxDetails = document.getElementsByClassName("meal-box");
+
+  ///// hiding previous meal-box divs for new search
+  if (mealBoxDetails.length > 0) {
+    for (let i = 0; i < mealBoxDetails.length; i++) {
+      const div = mealBoxDetails[i];
+      div.style.display = "none";
+    }
+  }
+
   const inputMealName = document.getElementById("input-name").value;
   fetchingMealData(inputMealName);
 });
 
+//////////// FETCHING FUNCTION ///////////
 const fetchingMealData = (inputNameValue) => {
   fetch(
     `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputNameValue}`
   )
     .then((response) => response.json())
     .then((data) => showMeal(data.meals));
+};
 
-  const showMeal = (meals) => {
-    meals.forEach((meal) => {
-      const mealName = meal.strMeal;
-      const mealThumbnail = meal.strMealThumb;
+///////////// FUNCTIONS /////////////
+const showMeal = (meals) => {
+  meals.forEach((meal) => {
+    const mealName = meal.strMeal;
+    const mealThumbnail = meal.strMealThumb;
 
-      const mealBox = document.getElementById("my-container");
-      const mealBoxDetails = document.createElement("div");
-      mealBoxDetails.className = "meal-box";
+    const mealBox = document.getElementById("my-container");
+    const mealBoxDetails = document.createElement("div");
+    mealBoxDetails.className = "meal-box";
 
-      const showInDom = `
-            <img class="meal-img" src="${mealThumbnail}">
-            <h4 class="meal-heading">${mealName}</h4>
-        `;
+    const showInDom = `
+          <img class="meal-img" src="${mealThumbnail}">
+          <h4 class="meal-heading">${mealName}</h4>
+      `;
 
-      mealBoxDetails.innerHTML = showInDom;
-      mealBox.appendChild(mealBoxDetails);
-    });
-  };
+    mealBoxDetails.innerHTML = showInDom;
+    mealBox.appendChild(mealBoxDetails);
+  });
 };
